5ffa6156964a32a917356eb4a785cb7491e32329
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2023-04-05 22:06:28 +0700

    change file

--- public/main.js
+++ public/main.js
@@ -5,11 +5,13 @@ const toggleMenuIcon = document.getElementById('tqd-toggle-top-menu-icon');
 document.addEventListener('click', (e) => {
    if(toggleMenuIcon.contains(e.target)){
         //click to toggle menu icon
+        
         topMenu.classList.toggle('hidden')
         topMenu.classList.toggle('tqd-topmenu-expanded')
 
    }else {
         //click outside
+
         if(topMenu.classList.contains('tqd-topmenu-expanded')){
             topMenu.classList.remove('tqd-topmenu-expanded');
             topMenu.classList.add('hidden')

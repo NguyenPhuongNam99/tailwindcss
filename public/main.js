9cfa041430ef9b9caa07fcfae0dce072e8ff0831
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2023-04-05 21:57:34 +0700

    responsive header

---
+++ public/main.js
@@ -0,0 +1,19 @@
+const topMenu = document.getElementById('tqd-top-menu');
+const toggleMenuIcon = document.getElementById('tqd-toggle-top-menu-icon');
+
+
+document.addEventListener('click', (e) => {
+   if(toggleMenuIcon.contains(e.target)){
+        //click to toggle menu icon
+        topMenu.classList.toggle('hidden')
+        topMenu.classList.toggle('tqd-topmenu-expanded')
+
+   }else {
+        //click outside
+        if(topMenu.classList.contains('tqd-topmenu-expanded')){
+            topMenu.classList.remove('tqd-topmenu-expanded');
+            topMenu.classList.add('hidden')
+        }
+
+   }
+})
